64256ef9a620df956e4b7b825230495cbf60a157
MaksRo52 <romancov_dzr@mail.ru>
2025-02-25 10:57:10 +0300

    add DRF

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main', 'users',
+    'main', 'users', 'rest_framework', 
 ]
 
 MIDDLEWARE = [
