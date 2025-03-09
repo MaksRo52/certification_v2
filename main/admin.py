9112a49bd3e1dcf44dd5e8eb971e779f632b574d
MaksRo52 <romancov_dzr@mail.ru>
2025-03-09 19:52:56 +0300

    add admins owner_link

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.urls import reverse
 
 from main.models import Commentary, Post
 from users.models import User
@@ -6,12 +7,19 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("username", "phone_number")
+    list_display = ("username", "email")
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title", "owner", "created_at")
+    list_display = ("title", "owner_link", "created_at")
+    list_filter = ("created_at",)
+
+    def owner_link(self, obj):
+        if obj.owner:
+            admin_url = reverse('admin:users_user_change', args=[obj.owner.id])
+            return admin.utils.format_html(f'<a href="{admin_url}">{obj.owner}</a>')
+        return "Автора нет"
 
 
 @admin.register(Commentary)
